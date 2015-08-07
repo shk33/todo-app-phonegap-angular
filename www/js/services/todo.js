c7b5c44a9bfabb68f6ee74a155729dc992cf16a3
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-07 16:06:40 -0500

    Add http request in todo service

--- www/js/services/todo.js
+++ www/js/services/todo.js
@@ -1,11 +1,28 @@
 angular.module('todoApp.services', [])
-.factory('Todos', function () {
-  var items = [
-    {text:'here is  my  first to  do',  done:true,  details:''},
-    {text:'continue writing chapter 1 for this  book',  done:false, 
-details:''},
-    {text:'work on  chapter 2 examples',  done:false, details:''}
-  ];
-
-  return items;
+.config(function ($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+.factory('Todos', function ($http) {
+  return {
+    getAll: function () {
+      //  return  $http.get('http://10.0.2.2:8000/todos');  //  if  using android
+      return $http.get('http://localhost:8000/todos');
+    },
+    getTodo: function (id) {
+      //  return  $http.get('http://10.0.2.2:8000/todos?id='+id); //  if  using android
+      return $http.get('http://localhost:8000/todos?id='+id);
+    },
+    save: function  (todoData)  {           
+      //  return  $http.post('http://10.0.2.2:8000/todos',  todoData);  //  if  using android
+      return  $http.post('http://localhost:8000/todos', todoData);
+    },
+    edit: function  (id,  todoData) {           
+      //  return  $http.post('http://10.0.2.2:8000/todos',  todoData);  //  if  ing android
+      return  $http.post('http://localhost:8000/todos?id='+id, todoData);
+        },
+    delete: function(id)  {
+      console.log(" i dont  think I have  a delete  here.")
+    }
+  };
 });
